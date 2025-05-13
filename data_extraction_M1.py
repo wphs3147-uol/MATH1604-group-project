e9edfd6677d52f6ea48b58efc4d65ea3051921b2
mm23jg-uol <mm23jg@leeds.ac.uk>
2025-05-13 21:15:36 +0000

    Added data_extraction_M1 and removed raw data

---
+++ data_extraction_M1.py
@@ -0,0 +1,23 @@
+def extract_answers_sequence(file_path):
+    with open(file_path, 'r') as file:
+        answers = file.readlines() # reads file in
+    boxlines = []
+    for line in answers:
+        line = line.strip()
+        if '[ ]' in line or '[x]' in line or '[X]' in line:
+            boxlines.append(line) # filters by line with check boxes
+    boxes = []
+    for line in boxlines:
+        boxes.append(line[:3]) # generates list of only checkboxes
+    ansnum = []
+    for i in range(0, len(boxes), 4):
+        qboxes = boxes[i:i+4]
+        if '[x]' in qboxes:
+            ansnum.append(qboxes.index("[x]")+1)
+        else:
+            ansnum.append(0) # iterates through boxes in chunks of 4 (for each question) generates list of answers with 0 if unanswered
+    return ansnum
+
+def write_answers_sequence(answers, id):
+    with open('answers_list_' + str(id) + '.txt', 'w') as file:
+        file.write(str(answers))
